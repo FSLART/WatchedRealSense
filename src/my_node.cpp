de5900eb7fecded7b5b4bb21bc7c334b8e221db8
André Silva <2201724@my.ipleiria.pt>
2023-11-25 18:18:35 +0000

    further implementation

--- src/my_node.cpp
+++ src/my_node.cpp
@@ -11,14 +11,25 @@ namespace carnary::client {
 class RealSenseNode : public rclcpp::Node {
 public:
     RealSenseNode() : Node("realsense_node"), carnary_client_() {
-        // Initialize RealSense pipeline
+        // Declare and get parameters
+        this->declare_parameter<std::string>("depth_topic", "depth");
+        this->declare_parameter<std::string>("rgb_topic", "rgb");
+        std::string depth_topic = this->get_parameter("depth_topic").as_string();
+        std::string rgb_topic = this->get_parameter("rgb_topic").as_string();
+
+       
         pipeline_.start();
 
-        // Create ROS2 publishers
-        depth_pub_ = create_publisher<sensor_msgs::msg::Image>("depth", 10);
-        rgb_pub_ = create_publisher<sensor_msgs::msg::Image>("rgb", 10);
+        
+        depth_pub_ = create_publisher<sensor_msgs::msg::Image>(depth_topic, 10);
+        rgb_pub_ = create_publisher<sensor_msgs::msg::Image>(rgb_topic, 10);
 
-        // Set up a timer for periodic heartbeats
+        // Timer for capturing and publishing RealSense data
+        capture_timer_ = create_wall_timer(std::chrono::milliseconds(100), [this]() {
+            capture_and_publish();
+        });
+
+        // Timer for periodic heartbeats
         handshake_timer_ = create_wall_timer(std::chrono::seconds(5), [this]() {
             perform_handshake();
         });
@@ -33,42 +44,49 @@ public:
     }
 
     void capture_and_publish() {
-        // Capture RealSense frames
-        rs2::frameset frames = pipeline_.wait_for_frames();
-        rs2::depth_frame depth_frame = frames.get_depth_frame();
-        rs2::video_frame color_frame = frames.get_color_frame();
+        try {
+           
+            rs2::frameset frames = pipeline_.wait_for_frames();
+            rs2::depth_frame depth_frame = frames.get_depth_frame();
+            rs2::video_frame color_frame = frames.get_color_frame();
 
-        // Convert RealSense frames to ROS2 messages
-        auto depth_msg = convert_to_ros_message(depth_frame);
-        auto rgb_msg = convert_to_ros_message(color_frame);
+            
+            auto depth_msg = convert_to_ros_message(depth_frame);
+            auto rgb_msg = convert_to_ros_message(color_frame);
 
-        // Publish RealSense data
-        depth_pub_->publish(depth_msg);
-        rgb_pub_->publish(rgb_msg);
+            
+            depth_pub_->publish(depth_msg);
+            rgb_pub_->publish(rgb_msg);
+        } catch (const std::exception& e) {
+            RCLCPP_ERROR(this->get_logger(), "Error in capture_and_publish: %s", e.what());
+        }
     }
 
     void perform_handshake() {
         try {
-            carnary_client_.ping(); // Send heartbeat to CARnary
+            carnary_client_.ping(); 
         } catch (const std::runtime_error& e) {
             RCLCPP_ERROR(this->get_logger(), "Error pinging CARnary: %s", e.what());
+            //TODO: Implement reconnection logic here
         }
     }
 
     ~RealSenseNode() {
-        carnary_client_.cleanup(); // Cleanup CARnary client
+        pipeline_.stop(); 
+        carnary_client_.cleanup(); 
     }
 
 private:
     rs2::pipeline pipeline_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr depth_pub_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr rgb_pub_;
+    rclcpp::TimerBase::SharedPtr capture_timer_;
     rclcpp::TimerBase::SharedPtr handshake_timer_;
     carnary::client::CARnaryClient carnary_client_;
 
-    // Method to convert RealSense frame to ROS2 message (implement as needed)
+    // Method to convert RealSense frame to ROS2 message
     sensor_msgs::msg::Image convert_to_ros_message(const rs2::frame& frame) {
-        // Conversion logic...
+        //TODO: Conversion logic...
     }
 };
 
@@ -79,3 +97,4 @@ int main(int argc, char* argv[]) {
     rclcpp::shutdown();
     return 0;
 }
+
