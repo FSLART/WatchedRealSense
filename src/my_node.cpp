6813feb54c5d71e465be7f480e1a4ed8b942eafa
André Silva <2201724@my.ipleiria.pt>
2023-11-27 17:27:27 +0000

    Camera Initialization Check

--- src/my_node.cpp
+++ src/my_node.cpp
@@ -24,26 +24,54 @@ using namespace carnary::client;
 class RealSenseNode : public rclcpp::Node {
 public:
     RealSenseNode() : Node("realsense_node") {
-        // Initialize RealSense pipeline
-        pipeline_.start();
+        try {
+            // Check if a RealSense device is connected
+            rs2::context ctx;
+            auto devices = ctx.query_devices();
+            if (devices.size() == 0) {
+                throw std::runtime_error("No RealSense devices connected");
+            }
 
-        // Declare and get parameters for topics
-        this->declare_parameter<std::string>("depth_topic", "depth");
-        this->declare_parameter<std::string>("rgb_topic", "rgb");
-        std::string depth_topic = this->get_parameter("depth_topic").as_string();
-        std::string rgb_topic = this->get_parameter("rgb_topic").as_string();
+            // Initialize RealSense pipeline
+            rs2::config cfg;
+            cfg.enable_stream(RS2_STREAM_DEPTH);
+            cfg.enable_stream(RS2_STREAM_COLOR);
+            pipeline_.start(cfg);
 
-        // Create ROS2 publishers
-        depth_pub_ = create_publisher<sensor_msgs::msg::Image>(depth_topic, 10);
-        rgb_pub_ = create_publisher<sensor_msgs::msg::Image>(rgb_topic, 10);
+            // Verify operational status by attempting to capture a frameset
+            rs2::frameset frames = pipeline_.wait_for_frames();
+            rs2::depth_frame depth_frame = frames.get_depth_frame();
+            rs2::video_frame color_frame = frames.get_color_frame();
+            if (!depth_frame || !color_frame) {
+                throw std::runtime_error("Failed to capture initial frameset from RealSense camera");
+            }
 
-        // Timer for capturing and publishing RealSense data
-        capture_timer_ = create_wall_timer(std::chrono::milliseconds(100), [this]() {
-            capture_and_publish();
-        });
+            // Declare and get parameters for topics
+            this->declare_parameter<std::string>("depth_topic", "depth");
+            this->declare_parameter<std::string>("rgb_topic", "rgb");
+            std::string depth_topic = this->get_parameter("depth_topic").as_string();
+            std::string rgb_topic = this->get_parameter("rgb_topic").as_string();
 
-        // Initialize connection to CARnary server
-        initialize_carnary_connection();
+            // Create ROS2 publishers
+            depth_pub_ = create_publisher<sensor_msgs::msg::Image>(depth_topic, 10);
+            rgb_pub_ = create_publisher<sensor_msgs::msg::Image>(rgb_topic, 10);
+
+            // Timer for capturing and publishing RealSense data
+            capture_timer_ = create_wall_timer(std::chrono::milliseconds(100), [this]() {
+                capture_and_publish();            
+            });
+
+            // Initialize connection to CARnary server
+            initialize_carnary_connection();
+
+        } catch (const rs2::error &e) {
+            RCLCPP_ERROR(this->get_logger(), "RealSense SDK error: %s", e.what());
+            // Handle RealSense SDK specific errors
+        } catch (const std::exception &e) {
+            RCLCPP_ERROR(this->get_logger(), "General error in RealSense initialization: %s", e.what());
+            // Handle other exceptions
+        }
+    }
     }
 
     void initialize_carnary_connection() {
